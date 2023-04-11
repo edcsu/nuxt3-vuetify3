1ef6b1f328415317a022a07a71a22c40ce28099f
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-11 16:57:36 +0300

    style: update app bar title

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
           </template>
 
-          <v-app-bar-title>Title</v-app-bar-title>
+          <v-app-bar-title>Nuxt 3 Vuetify 3 Sample</v-app-bar-title>
 
           <v-spacer></v-spacer>
 
